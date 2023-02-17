d106f992ea3ce744b08af7bebefd580512776846
Otávio Rocha <94090679+OitavoRocha@users.noreply.github.com>
2023-02-17 15:39:16 -0300

    Update ex31.c

--- Recursao/ex31.c
+++ Recursao/ex31.c
@@ -1,10 +1,8 @@
 /*
 Uma palavra de Fibonacci e definida por
-
 f(n) = b se n = 0
 f(n) = a se n = 1
 f(n) = f(n-1)+f(n-2) se n > 1
-
 Aqui o sımbolo “+” denota a concatenacao de duas strings. Esta sequencia inicia com as seguintes palavras:
 b, a, ab, aba, abaab, abaababa, abaababaabaab, ...
 Faca uma funcao recursiva que receba um numero N e retorne a N-esima palavra de Fibonacci.
@@ -12,7 +10,7 @@ Faca uma funcao recursiva que receba um numero N e retorne a N-esima palavra de 
 #include<stdio.h>
 #include<string.h>
 
-char passo(int n);
+char *passo(int n);
 
 int main() {
     int n;
@@ -25,12 +23,12 @@ int main() {
     return 0;
 }
 
-char passo(int n) {
+char *passo(int n) {
     //ISSO AQUI TA CERTO? APARENTEMENTE NAO
     if ( n==1 ) {
-        return 'b';
+        return "b";
     } else if ( n==0 ) {
-        return 'a';
+        return "a";
     } else {
         return strcat(passo(n-1), passo(n-2));
     }
