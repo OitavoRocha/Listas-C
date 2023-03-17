2f845242314633b65eef2e2ce2aec852870badc4
Otávio Rocha <94090679+OitavoRocha@users.noreply.github.com>
2023-03-17 13:04:52 -0300

    Update main.c

--- pilha/src/main.c
+++ pilha/src/main.c
@@ -16,11 +16,34 @@ typedef struct _node {
 
 Stack * START();
 void PUSH( Stack * pStack, Node * newElement );
+Node * POP( Stack * pStack );
+void PRINT( Stack * pStack );
 Node * NEW( int code );
+void CLEAR( Stack * pStack );
 BOOL EMPTY( Stack * pStack );
 
 int main() {
     Stack * pS = START();
+    Node * pPopped;
+
+    PUSH( pS, NEW(1));
+    PUSH( pS, NEW(2));
+    PUSH( pS, NEW(3));
+    PUSH( pS, NEW(4));
+    PUSH( pS, NEW(5));
+    PUSH( pS, NEW(6));
+
+    PRINT( pS );
+
+    pPopped = POP( pS );
+    free( pPopped );
+    pPopped = POP( pS );
+    free( pPopped );
+
+    printf("\n+----+\n\n");
+    PRINT( pS );    
+
+    CLEAR( pS );
 
     return 0;
 }
@@ -29,11 +52,44 @@ Stack * START() {
     Stack * pStack = ( Stack * )malloc( sizeof( Stack ) );
 
     pStack->top = NULL;
+    return pStack;
 }
 
 void PUSH( Stack * pStack, Node * newElement ) {
     newElement->next = pStack->top;
     pStack->top = newElement;
+}
+
+Node *POP( Stack * pStack ) {
+    Node * pNode;
+
+    if ( EMPTY( pStack ) ) {
+        printf("Lista vazia/n");
+        return NULL;
+    }
+
+    pNode = pStack->top;
+    pStack->top = pNode->next;
+
+    return pNode;
+}
+
+void PRINT( Stack * pStack ) {
+    Stack * pTemporary = START();
+    Node * pNode;
+
+    while ( pStack->top ) {
+        pNode = POP( pStack );
+        PUSH( pTemporary, pNode );
+        printf(" %d \n", pNode->code);
+    }
+
+    while ( pTemporary->top ) {
+        pNode = POP( pTemporary );
+        PUSH( pStack, pNode );
+    }
+
+    free( pTemporary );
 }
 
 Node * NEW( int code ) {
@@ -43,6 +99,17 @@ Node * NEW( int code ) {
     pNode->next = NULL;
 
     return pNode;
+}
+
+void CLEAR( Stack * pStack ) {
+    Node * pNode;
+
+    while ( pStack->top ) {
+        pNode = POP( pStack );
+        free( pNode );
+    }
+
+    free( pStack );
 }
 
 BOOL EMPTY( Stack * pStack ) {
